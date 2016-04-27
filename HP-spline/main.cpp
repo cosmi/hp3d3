4db7a762e67a6e93e6b4a7e5b1c1d4ccb6faeea8
Marcin Skotniczny <marcin@swmansion.com>
2016-04-27 20:13:28 +0200

    Initial Commit

---
+++ HP-spline/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HP-spline
+//
+//  Created by Marcin on 27.04.2016.
+//  Copyright © 2016 Marcin Skotniczny. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
